c5c7917ee854c1fcf70ca90b34ae608b1412ee32
Mariusz Borkowski <mariuszborkowski1988@protonmail.com>
2025-07-23 08:17:05 +0200

    Logowanie i Rejestracja, encja Kategorii.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,9 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/Entity/Category.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?bool $add_favourites = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $user_role = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function isAddFavourites(): ?bool
+    {
+        return $this->add_favourites;
+    }
+
+    public function setAddFavourites(bool $add_favourites): static
+    {
+        $this->add_favourites = $add_favourites;
+
+        return $this;
+    }
+
+    public function getUserRole(): ?string
+    {
+        return $this->user_role;
+    }
+
+    public function setUserRole(string $user_role): static
+    {
+        $this->user_role = $user_role;
+
+        return $this;
+    }
+}
