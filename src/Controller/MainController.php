2f980e4872a3edfa1272f77d297a74deea1d982a
Mariusz Borkowski <mariuszborkowski1988@protonmail.com>
2025-07-23 21:47:22 +0200

    Log out, sprawdzanie czy zalogowany.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,8 +11,10 @@ final class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'controller_name' => 'MainController'
         ]);
     }
 }
