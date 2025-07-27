0d6d18e9b573edf80c30833d64d030c97e11b6a8
Mariusz Borkowski <mariuszborkowski1988@protonmail.com>
2025-07-27 10:07:07 +0200

    Stylowanie formularza rejestracji, sprawdzanie czy uytkownik jest zalogowany.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,6 +12,12 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+
+        /* Check login user */
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_main');
+        }
+
         // get the security error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
